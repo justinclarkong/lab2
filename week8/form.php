9e8b549e42e944fbc7d55c3297ac5fea76f42ccf
Justin Clark Ong <justinclarkong@gmail.com>
2024-02-06 08:16:02 +0800

    form.php: use socitcloud db

--- week8/form.php
+++ week8/form.php
@@ -115,3 +115,33 @@ echo $gender;
 		<script src="scripts.js"></script>
 	</body>
 </html>
+
+ <?php
+//$servername = "localhost";
+//$username = "root";
+//$password = "";
+//$dbname = "myDB";
+
+$servername = "localhost";
+$username = "webprogmi222_sf221";
+$password = "xE*Y2nleNVvZm[!!";
+$dbname = "webprogmi22_sf221";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO jhong_myguests (name, email, website, comment, gender)
+VALUES ('$name', '$email', '$website', '$comment', '$gender')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
