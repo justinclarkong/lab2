32b56ec526c1ac53354c1939db90eb765c00ee41
Justin Clark Ong <justinclarkong@gmail.com>
2024-02-06 08:24:21 +0800

    form.php: fix typo

--- week8/form.php
+++ week8/form.php
@@ -125,7 +125,7 @@ echo $gender;
 $servername = "localhost";
 $username = "webprogmi222_sf221";
 $password = "xE*Y2nleNVvZm[!!";
-$dbname = "webprogmi22_sf221";
+$dbname = "webprogmi222_sf221";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
